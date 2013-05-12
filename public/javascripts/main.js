c18a455d2fe08f6c16b2c20075716c9534e99d46
Aman Kapur <amankapur91@gmail.com>
2013-05-12 18:24:08 -0400

    room sockets work

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,26 +3,34 @@ $(function () {
 
   window.pathArray = document.URL.split( '/' );
   host = pathArray[0] + '//' + pathArray[2]
-
+  console.log(host);
   var socket = io.connect(host);
   
+  // console.log(socket);
   socket.on('roomcreate', function (data) {
 
-  	getRooms();
-    
+    console.log('ROOM CREATE RECEIVED');
+    if (typeof(data.url) !== "undefined")
+    {
+      redirect(data.url);
+    }
+    else {
+    	getRooms();
+    }
   });
 
 
   var getRooms = function(){
   	$.get('/rooms/show_all', function(data){
-  		
+  		console.log(data);
   		$("#roomlist").html(data);
-  		// console.log(data);
+  		
   	});
   }
 
-  $("body").on('click', '#newRoomForm', function(){
-      
+  $("body").on('click', '#newRoomForm', function(e){
+    event.preventDefault();
+
     data_name = $("#nameinput").val();
     data_uid = $("#uid").val();
     $.post('/rooms/create', {name: data_name, uid: data_uid}, function(data){
@@ -30,10 +38,17 @@ $(function () {
         $("#createform").prepend(data);
       }
       if (data.indexOf('/room') > -1){
-        window.location = host + data;
+        socket.emit('roomcreate', {url: host + data});
+        
+        // redirect(host + data);
       }
-    }); 
-    socket.emit('roomcreate', {});
+    
+  });
+
 });
 
 });
+
+function redirect(url){
+  window.location = url;
+}
